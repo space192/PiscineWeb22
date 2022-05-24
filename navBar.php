3aac39fb1ba182ffb864cf6e7e9590e5dfc7d78e
pierrelouispaillet <57723732+pierrelouispaillet@users.noreply.github.com>
2022-05-24 22:56:00 +0200

    ajout médecins spécialistes

--- navBar.php
+++ navBar.php
@@ -7,7 +7,7 @@
                 <button onclick="menuDeroulant()" class="boutounDeroulant">Tout Parcourir</button>
                 <div id="Options" class="contenu">
                     <a href='Medecins.php'>Médecin généraliste</a>
-                    <a href="#">Médecin spécialiste</a>
+                    <a href='MedecinsS.php'>Médecin spécialiste</a>
                     <a href="#">Laboratoire</a>
                 </div>
             </div>
