f1669e200371ef460b5e75e0c0f057bd2dc019c5
Ahddry <78737719+Ahddry@users.noreply.github.com>
2022-05-25 15:43:29 +0200

    Début recherche CSS

---
+++ Recherche.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>OMNES Santé : Mon compte</title>
+    <meta name="description" content="Mon compte Omnes Santé">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="Index.css">
+    <link rel="stylesheet" href="Recherche.css">
+    <link rel="icon" href="images/Decor/LogoOmnesSante2.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/c6c9e611bb.js" crossorigin="anonymous"></script>
+    <?php include_once 'const.php' ?>
+    <script src="Index.js"></script>
+</head>
+
+<body>
+    <div id="Wrapper">
+        <header id="Header">
+            <?php include 'navBar.php' ?>
+            <?php include 'NavBarStatique.php' ?>
+            <div id="Darkmode"><button onclick="switchTheme()" title="Changer de thème de couleur">◐</button></div>
+        </header>
+        <section id="Milieu">
+            <div class="Boites">
+                <h1>Résultat de la recherche</h1>
+                <p>
--- querySearch.php
+++ querySearch.php
@@ -1,6 +1,7 @@
+<?php include 'Recherche.php' ?>
 <?php
 include_once 'const.php';
-if(isset($_POST["search"]))
+if(isset($_POST["search"]) && $_POST["search"] != "")
 {
     global $mysqlConnection;
     $temp = explode(" ", $_POST["search"]);
@@ -17,19 +18,32 @@ if(isset($_POST["search"]))
     $memberStatement->execute();
     $resultM = $memberStatement->fetchAll();
     //recherche dans les services
-    $sql = "SELECT Nom,Info FROM Service WHERE LOWER(Nom) LIKE '%" . $_POST["search"] . "%';";
+    $sql = "SELECT Nom,Info,Regles FROM Service WHERE LOWER(Nom) LIKE '%" . $_POST["search"] . "%';";
     $memberStatement = $mysqlConnection->prepare($sql);
     $memberStatement->execute();
     $resultS = $memberStatement->fetchAll();
+    echo("<br>");
+    echo("<h2>Sercives</h2>");
     foreach($resultS as $res)
     {
-        echo($res["Nom"] . " " . $res["Info"] . "<br/><br/><br/><br/>");
+        echo('<div class="SousBoites" style="text-align:left!important">');
+        echo("<h4>".$res["Nom"] . "</h4><br> " . str_replace("\n", "<br>", $res["Info"]) . "<br><br>". str_replace("\n", "<br>", $res["Regles"]) . "<br/><br/><br/><br/>");
+        echo('</div><br>');
+    }
+    echo("<br>");
+    echo("<h2>Médecins</h2>");
+    foreach($resultM as $res)
+    {
+        echo('<div class="SousBoites" style="text-align:left!important">');
+        echo("<h4>".$res["Prenom"] . " " . $res["Nom"] . "</h4><br> " . $res["Localisation"] . " <br>" . $res["Telephone"] . "<br> ". $res["Mail"] . "<br> " . " <img src='" . $res["pp"] . "'> "."<br/><br/><br/><br/>");
+        echo('</div><br>');
     }
 }
 else
 {
     die;
 }
+echo("</p></div> </section> </div> </body> </html>")
 
 
 ///tu peux faire le front ici pour l'affichage des recherches on aura les variables du dessus pour afficher les résultats
