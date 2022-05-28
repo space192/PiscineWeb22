d11f113cc1308bb6b3382933d29943fd7eaa145d
Arthur Fournier <arthur.g.fournier@gmail.com>
2022-05-28 15:51:34 +0200

    Update MedecinsS.php

--- MedecinsS.php
+++ MedecinsS.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style2.css">
     <link rel="stylesheet" href="Index.css">
-    <script src="Index.js"></script>
+    <script src="index.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <link rel="icon" href="images/Decor/LogoOmnesSante2.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
